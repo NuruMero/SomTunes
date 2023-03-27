1a904c4752bd61cd36a06005ad4801f2a81eba89
NuruMero <adanguerreromesa@gmail.com>
2023-03-27 15:33:46 +0200

    Adding @ControlleAdvice to ResponseEntity exceptions class

---
+++ src/main/java/com/example/song/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.song.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String body = "ERROR: Illegal argument or state introduced in the API.";
+        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
